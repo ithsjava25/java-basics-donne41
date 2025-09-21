30d7ab40a5304f456702302a970b8a9ece37416d
danne <xsz300@gmail.com>
2025-09-21 21:49:00 +0200

    bättre hantering med args, och säkrare datum val.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elAPI = new ElpriserAPI();
-        LocalDate idag = LocalDate.parse(LocalDate.now().toString());
+
 //        System.out.print(elAPI.getPriser(datum, ElpriserAPI.Prisklass.SE4));
         //klassen fattade inte LocalDate format, tvunget med parse toString.
 
@@ -25,72 +25,66 @@ public class Main {
         //bättre input hantering
 
 
-        List<String> validInput = Arrays.asList("SE1", "SE2", "SE3", "SE4");
+        List<String> validZone = Arrays.asList("SE1", "SE2", "SE3", "SE4");
+        String validDate = "202";
         String help;
         String zone = null;
         LocalDate askDate = null;
         LocalDate callDate = null;
         boolean valid = false;
+        double sumPris = 0;
+        double avePris = 0;
 
         for (String arg : args) {
-            if (validInput.contains(arg.toUpperCase())) {
-                zone = arg.toUpperCase();
+            if (validZone.contains(arg.toUpperCase())) {
+                zone = arg;
                 zone = zone.toUpperCase();
                 System.out.println(zone);
                 valid = true;
             }
-            try {
-                callDate = LocalDate.parse(arg, DateTimeFormatter.ISO_LOCAL_DATE);
-            } catch (DateTimeParseException e) {
+            if (arg.contains(validDate)) {
+                try {
+                    callDate = LocalDate.parse(arg, DateTimeFormatter.ISO_LOCAL_DATE);
+                } catch (DateTimeParseException e) {
+                    callDate = LocalDate.parse(java.time.LocalDate.now().toString());
+                    //falskt datum, använd dagens datum.
+                }
+            }
+            if (valid == true && callDate == null) {
                 callDate = LocalDate.parse(java.time.LocalDate.now().toString());
-                //inget datum, använd dagens datum.
             }
         }
         if (valid) {
-            System.out.println("PRINTINT ARGS AND TODAY" + idag);
             List<ElpriserAPI.Elpris> testPriser = elAPI.getPriser(callDate, ElpriserAPI.Prisklass.valueOf(zone));
             if (testPriser == null) {
                 System.out.println("Kunde inte hitta priser");
-            } else {
-                System.out.println("Dagens priser" + testPriser.size());
-                testPriser.stream().limit(4).forEach(pris ->
-                        System.out.printf("Tid: %s, Pris: %.4f SEK/kWh\n",
-                                pris.timeStart().toLocalTime(), pris.sekPerKWh()));
-            }
-        } else {
-            System.out.println("Fann inte någon elzone i argument, startar manuell input.");
-            manuellInput();
-        }
-//        for (int i = 0; i<inputArray.length; i++){
-//            String s = inputArray[i];
-//            try {
-//                askDate = LocalDate.parse(s, DateTimeFormatter.ISO_LOCAL_DATE);
-//            } catch (DateTimeParseException e) {
-//                // inte ett datum, hoppa vidare
-//            }
-//            if (s.equals("--help")||s.isBlank()) {
-//                System.out.printf("""
-//                    To use this program, input --help for this information screen.
-//                    Input a elzone SE1,SE2,SE3 or SE4 and a date to see
-//                    Average price for the day
-//                    Highest price and what hour
-//                    lowest price and what hour.
-//                    if date is not included, current day is used.""");
-//            }else if(s.equals("--ZONE")){
-//                ElpriserAPI.Prisklass.valueOf(inputArray[i]);
-//
-//                askDate = LocalDate.parse(input);
-//                List<ElpriserAPI.Elpris>priser = elAPI.getPriser(askDate, ElpriserAPI.Prisklass.valueOf(inputArray[i + 1]));
-//                System.out.print(priser);
-//            }else if(s.equals("--DATE")){
-//                break;
-//            }
-//
-//        }
-//        if(inputArray[0].equals("--help")) {
-//            System.out.println("Help executed, with date: " + datum);
-//        }
+                System.out.println("startar manuell input.");
+                manuellInput();
 
+            } else {
+                testPriser.sort(Comparator.comparing(ElpriserAPI.Elpris::sekPerKWh));
+                var highPris = testPriser.getLast().sekPerKWh();
+                var highPrisTidStart = testPriser.getLast().timeStart().toLocalTime().toString().substring(0, 2);
+                var highPrisTidSlut = testPriser.getLast().timeEnd().toLocalTime().toString().substring(0, 2);
+                var lowPris = testPriser.getFirst().sekPerKWh();
+                var lowPrisTidStart = testPriser.getFirst().timeStart().toLocalTime().toString().substring(0, 2);
+                var lowPrisTidSlut = testPriser.getFirst().timeEnd().toLocalTime().toString().substring(0, 2);
+                for (int i = 0; i < testPriser.size(); i++) {
+                    sumPris += testPriser.get(i).sekPerKWh();
+                }
+                //fixa tiden så att start och slut tiden är tex 01-02 istället för 01:00-02:00
+                //skriv ut i öre istället för kr.
+                avePris = sumPris / testPriser.size();
+                double highPrisOre = highPris * 100;
+                double lowPrisOre = lowPris * 100;
+                double avePrisOre = avePris * 100;
+                System.out.printf("""
+                        Högsta pris:  %.4f öre/kWh  Tid: %s-%s
+                        Lägsta pris:  %.4f öre/kWh  Tid: %s-%s
+                        Medelpris:    %.4f öre/kWh \n""", highPrisOre, highPrisTidStart, highPrisTidSlut, lowPrisOre, lowPrisTidStart, lowPrisTidSlut, avePrisOre);
+
+            }
+        }
     }
 
     public static void manuellInput() {
@@ -100,6 +94,7 @@ public class Main {
         String zone = null;
         String callDate = null;
         double avePris = 0;
+        double sumPris = 0;
         do {
             String[] input = sc.nextLine().toUpperCase().split(" ");
             for (int i = 0; i < input.length; i++) {
@@ -111,6 +106,7 @@ public class Main {
                     helpPrint();
                 } else if (input[i].equals("--ZONE")) {
                     zone = input[i + 1];
+                    i++;
                     validInput = true;
                 } else if (input[i].equals("--DATE")) {
                     try {
@@ -118,11 +114,12 @@ public class Main {
                     } catch (DateTimeParseException e) {
                         callDate = LocalDate.now().toString();
                     }
-                } else if (validInput == true && callDate == null) {
-                    callDate = LocalDate.now().toString();
                 } else {
                     System.out.println("Zone not included as requierd. type --help for more information");
                 }
+            }
+            if (validInput == true && callDate == null) {
+                callDate = LocalDate.now().toString();
             }
         } while (!validInput);
 
@@ -132,26 +129,34 @@ public class Main {
         } else {
             testPriser.sort(Comparator.comparing(ElpriserAPI.Elpris::sekPerKWh));
             var highPris = testPriser.getLast().sekPerKWh();
-            var highPrisTid = testPriser.getLast().timeStart().toLocalTime();
+            var highPrisTidStart = testPriser.getLast().timeStart().toLocalTime().toString().substring(0, 2);
+            var highPrisTidSlut = testPriser.getLast().timeEnd().toLocalTime().toString().substring(0, 2);
             var lowPris = testPriser.getFirst().sekPerKWh();
-            var lowPrisTid = testPriser.getFirst().timeStart().toLocalTime();
-            for (int i = 0; i < testPriser.size(); i++){
-                double sumPris = testPriser.get(i).sekPerKWh();
-                avePris = sumPris/testPriser.size();
+            var lowPrisTidStart = testPriser.getFirst().timeStart().toLocalTime().toString().substring(0, 2);
+            var lowPrisTidSlut = testPriser.getFirst().timeEnd().toLocalTime().toString().substring(0, 2);
+            for (int i = 0; i < testPriser.size(); i++) {
+                sumPris += testPriser.get(i).sekPerKWh();
             }
+            //fixa tiden så att start och slut tiden är tex 01-02 istället för 01:00-02:00
+            //skriv ut i öre istället för kr.
+            avePris = sumPris / testPriser.size();
+            double highPrisOre = highPris * 100;
+            double lowPrisOre = lowPris * 100;
+            double avePrisOre = avePris * 100;
             System.out.printf("""
-                Highest price: %.4f krTid: %s 
-                Lowest price:  %.4f krTid: %s
-                average price: %.4f kr""", highPris, highPrisTid, lowPris, lowPrisTid, avePris);
+                    Högsta pris:  %.4f öre/kWh  Tid: %s-%s
+                    Lägsta pris:  %.4f öre/kWh  Tid: %s-%s
+                    Medelpris:    %.4f öre/kWh  \n""", highPrisOre, highPrisTidStart, highPrisTidSlut, lowPrisOre, lowPrisTidStart, lowPrisTidSlut, avePrisOre);
 
+        }
 
 //            System.out.println("Dagens priser" + testPriser.size());
 //            testPriser.stream().forEach(pris ->
 //                    System.out.printf("Tid: %s, Pris: %.4f SEK/kWh\n",
 //                            pris.timeStart().toLocalTime(), pris.sekPerKWh()));
 //        }
-        }
     }
+
 
     public static void helpPrint() {
         System.out.printf("""
@@ -159,3 +164,4 @@ public class Main {
                 NOT YET FINISHED AS SEEN HERE """);
     }
 }
+
